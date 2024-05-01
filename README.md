412dbaa03b1856afb2a6c35525d91ae4baf511b4
Clara <claraalvarescamargo@gmail.com>
2024-04-30 21:00:27 -0300

    atualizando repositório

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # PROJETO_SPRINT_5
 Projeto Sprint 5 _ Bootcamp Data Analyst
+
+Aplicativo para visualização dos dados do DataFrame 'vehicles.csv', contendo informações sobre anúncios de carros.
+O aplicativo composto por:
+Seção 1: 
+1.1 visuzaliação do DataFrame
+1.2 filtragem do DataFrame por meio de widgets de seleção
+Seção 2: visualização dos histogramas de variáveis numéricas contínuas.
+Seção 3: visualização de gráfico de dispersão relacionando as variáveis mostradas na seção 2.
---
+++ app.py
@@ -0,0 +1,86 @@
+# importando as bibliotecas necessárias
+import streamlit as st
+from st_aggrid import AgGrid
+import pandas as pd
+import streamlit_pandas as sp
+
+# título
+st.title(':blue[Vehicle advertisements data]')
+
+# introdução
+st.markdown('''
+    This is an application for data visualization.''')
+
+
+# lendo os dados
+data_vehicles = pd.read_csv('vehicles.csv')
+
+
+# visualizando e filtrando os dados
+# definindo formato dos widgets de filtragem
+create_data = {"model_year": "multiselect",
+               "model": "multiselect",
+               "condition": "multiselect",
+               "cylinders": "multiselect",
+               "fuel": "multiselect",
+               "transmission": "multiselct",
+               "type": "select",
+               "paint_color": "multiselect"
+                }
+
+all_widgets = sp.create_widgets(data_vehicles, create_data, ignore_columns=["date_posted", "days_listed", "is_4wd"])
+res = sp.filter_df(data_vehicles, all_widgets)
+st.header("DataFrame")
+st.write(data_vehicles)
+
+st.header("Filtered DataFrame")
+st.write(res)
+
+
+# adicionando cabeçalho da seção
+st.subheader('Histograms')
+
+# criando caixas de seleção
+price = st.checkbox('Price')
+odometer = st.checkbox('Odometer')
+
+if price: # se a caixa de seleção for clicada
+    # escrevendo uma mensagem
+    st.write ('Creating a histogram for the column: price.')
+    
+    # criando um histograma
+    fig = px.histogram(data_vehicles, x='price')
+    
+    # exibindo o histograma
+    st.plotly_chart(fig, use_container_width=True)
+
+elif odometer:
+    # escrevendo uma mensagem
+    st.write ('Creating a histogram for the column: odometer.')
+    
+    # criando um histograma
+    fig = px.histogram(data_vehicles, x='odometer')
+    
+    # exibindo o histograma
+    st.plotly_chart(fig, use_container_width=True)
+
+else:
+    print ('Select a column to view a histogram.')
+    
+    
+    
+# adicionando cabeçalho da seção
+st.subheader('Scatter plot')
+
+# criando um botão
+price_odometer_button = st.button('Price vs Odometer')  
+
+if price_odometer_button:
+    # escrevendo uma mensagem
+    st.write ('Creating a scatter plot for the columns: odometer vs price.')
+    
+    # criando um gráfico de dispersão
+    fig = px.scatter(data_vehicles, x="odometer", y="price")
+
+    # exibindo o gráfico
+    st.plotly_chart(fig, use_container_width=True)
